34ff65640da7b751cb0e6099bb9e00e9ac33154a
Karol Żuraniewski <zuraniewski.karol@gmail.com>
2023-01-14 15:43:58 +0100

    layout adjust

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,8 +3,15 @@ import type { PropsWithChildren } from 'react';
 
 export default function Layout({ children }: PropsWithChildren) {
 	return (
-		<Box sx={{ backgroundColor: 'rgba(0, 0, 0, 0.03)', py: 5, px: 1 }}>
-			<Box sx={{ margin: '0 auto' }}>
+		<Box
+			sx={{
+				backgroundColor: 'rgba(0, 0, 0, 0.03)',
+				py: 5,
+				px: 1,
+				minHeight: '100vh',
+			}}
+		>
+			<Box sx={{ margin: '0 auto', maxWidth: 800 }}>
 				<Typography variant="h1" align="center" mb={5}>
 					Symulator Intel 8086
 				</Typography>
